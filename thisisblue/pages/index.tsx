5744f38376d91dd3e6729302b51a2acc83b75137
Blu3Jund <benjaminshawki@gmail.com>
2022-08-04 15:50:44 +0200

    First commit, just finished the browser Google Sign in button. Firebase registration of the user will follow shortly

---
+++ thisisblue/pages/index.tsx
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css'
+
+import Loader from '../components/loader'
+import toast from "react-hot-toast";
+
+export default function Home() {
+  return (
+      <div>
+          <button onClick={() => toast.success('hello toast!')}>
+              Toast me
+          </button>
+        {/*<Loader show />*/}
+      </div>
+    // <div className={styles.container}>
+    //
+    // </div>
+  )
+}
